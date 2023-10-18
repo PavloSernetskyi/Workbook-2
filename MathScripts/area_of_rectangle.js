75814017a9d6b1f19ad4a9b87f85709db5f05982
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-18 15:37:11 -0700

    exercises p.59
    exercises p.59

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let width = 20;
+let height = 30;
+let area = width * height;
+console.log(`The area of the rectangle is ${area}`); 
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let message = "calculating net worth";
+console.log(message);
+
+let assets = 20000;
+let liabilities = 15000;
+let result = assets - liabilities;
+console.log(`My net worth is ${result}`);
