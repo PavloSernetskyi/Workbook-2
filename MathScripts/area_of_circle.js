fc063d1b93320fb782dfe3c10957aec4ee183b27
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-18 15:57:21 -0700

    working on math scripts exercises
    working on math scripts exercises

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,6 @@
+"use strict"
+
+let circleRadius = 5;
+let areaOfCirlce = Math.PI * circleRadius ** 2;
+
+console.log(`The area of a circle with circle radius ${circleRadius} is ${areaOfCirlce}`);
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -1,2 +1,6 @@
 "use strict"
 
+let dish = 25;
+let rate = 0.10; // 10%
+let tip = dish * rate;
+console.log(`The tip on $${dish} food bill is $${tip} dollars.`);
