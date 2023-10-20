d5a31878c33444770429e385f3dd9b05b5ba156e
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-20 16:03:48 -0700

    complex-taxes

--- IfScripts/complex_taxes.js
+++ IfScripts/complex_taxes.js
@@ -1,23 +1,34 @@
 "use strict"
 
-let num1 = 2;
-let num2 = "2"
+// let num1 = 2;
+// let num2 = "2"
 
 // == allows to compare int and string?
-if (num1 == num2)
-{
-    console.log(`2 == '2' is True`);
-}
-else
-{
-    console.log(`False`);
-}
+// if (num1 == num2)
+// {
+//     console.log(`2 == '2' is True`);
+// }
+// else
+// {
+//     console.log(`False`);
+// }
 
-if (num1 === num2)
-{
-    console.log(`True`);
-}
-else
-{
-    console.log(`2 === '2' is False`);
-}
+// if (num1 === num2)
+// {
+//     console.log(`True`);
+// }
+// else
+// {
+//     console.log(`2 === '2' is False`);
+// }
+
+let weeklySalary = 2500;
+let taxRate = 0.23; // 23% federal taxes.
+let monthlyTaxes = weeklySalary * taxRate;
+
+console.log(`Your monthly earning are $${weeklySalary}.`);
+console.log(`Monthly taxes are $${monthlyTaxes}.`);
+
+let weeklyGrossPay = weeklySalary - monthlyTaxes;
+console.log(`Weekly gross pay is $${weeklyGrossPay}.`);
+
--- MathScripts/taxes.js
+++ MathScripts/taxes.js
@@ -3,5 +3,9 @@
 let monthlySalary = 5500;
 let taxRate = 0.23; // 23% federal taxes.
 let monthlyTaxes = monthlySalary * taxRate;
+
+console.log(`Your monthly earning are $${monthlySalary}.`);
 console.log(`Monthly taxes are $${monthlyTaxes}.`);
 
+let grossPay = monthlySalary - monthlyTaxes;
+console.log(`Gross monthly pay is $${grossPay}.`);
