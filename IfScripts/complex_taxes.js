e1216a82c9c32dcf273e02c4434996917cc4f47f
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-20 14:23:32 -0700

    update

--- IfScripts/complex_taxes.js
+++ IfScripts/complex_taxes.js
@@ -1,2 +1,23 @@
 "use strict"
 
+let num1 = 2;
+let num2 = "2"
+
+// == allows to compare int and string?
+if (num1 == num2)
+{
+    console.log(`2 == '2' is True`);
+}
+else
+{
+    console.log(`False`);
+}
+
+if (num1 === num2)
+{
+    console.log(`True`);
+}
+else
+{
+    console.log(`2 === '2' is False`);
+}
