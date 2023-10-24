fd13bebadb17979c9d042a14171307aff0c428f6
Pavlo_Sernetskyi <pavlosernetskyi@yahoo.com>
2023-10-23 23:40:28 -0700

    save point

--- WB2-exercises/GreetByName/script/index.js
+++ WB2-exercises/GreetByName/script/index.js
@@ -7,3 +7,16 @@ function init() {
 function onHelloBtnClicked() {
     alert("Hi there!");
 }
+
+
+//////////////////////////////////////////
+
+// // We would want this code to run when the user clicks the Show button
+// const nameField = document.getElementById("nameField");
+// const ageField = document.getElementById("ageField");
+// let name = nameField.value;
+// let age = ageField.value; // age is a string here
+// let message =
+//     `Hi ${name}! I hear you are ${age} years old!`;
+// const messagePara = document.getElementById("messagePara");
+// messagePara.innerHTML = message
